634a3db8877b77bb2dbea3265f52aba7909f622b
rishabhgithubrit <119465076+rishabhgithubrit@users.noreply.github.com>
2023-03-19 16:29:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// const weatherApi{
+//     key:  "dbce028471da19bed6a1b87d66387b29",
+//     baseUrl: http://api.openweathermap.org/data/2.5/weather?:
+// }
+// https://api.openweathermap.org/data/2.5/weather?q={}&appid=dbce028471da19bed6a1b87d66387b29
+
+// const f = () => {
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=dbce028471da19bed6a1b87d66387b29")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+// }
+// f() 
+// b7255442580e8019909aed0f8481eeb7
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// const weatherApi = {
+//     key : "b7255442580e8019909aed0f8481eeb7",
+//     baseUrl : "api.openweathermap.org/data/2.5/weather"
+// }
+// const searchInpbox = document.getElementById("inp-box")
+// searchInpbox.addEventListener('keypress',(event) =>{
+//     if(event.keyCode == 13){
+//         console.log(searchInpbox.value)
+//         getWeatherReport(searchInpbox.value)
+//     }
+// });
+// function getWeatherReport(city) {
+//     fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+//     .then(weather => {
+//         return weather.json();
+//     }).then(showWeatherReport);
+// }
+// function showWeatherReport(weather){
+//     console.log(weather)
+// }
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+const weatherApi = {
+    key: "bab281d79e5f1e9755a68d754cc313e7",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather", 
+}
+
+const searchInputBox = document.getElementById('input-box');
+
+
+searchInputBox.addEventListener('keypress', (event) => {
+    
+    if(event.keyCode == 13) {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display = "block";
+    }
+
+});
+
+
+function getWeatherReport(city) {
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather => {
+        return weather.json();
+    }).then(showWeatherReport);
+}
+
+
+function showWeatherReport(weather){
+    console.log(weather);
+
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+}
+
+
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+}
